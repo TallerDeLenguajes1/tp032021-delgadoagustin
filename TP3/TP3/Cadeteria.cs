c9ff32e4ca2402de165f94b14ddb219b31eb378d
Agustin Delgado <delgadoagustin147@gmail.com>
2021-09-01 18:41:31 -0300

    Crea

---
+++ TP3/TP3/Cadeteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    public class Cadeteria
+    {
+        string nombre;
+        List<Cadete> listaCadetes;
+    }
+}
