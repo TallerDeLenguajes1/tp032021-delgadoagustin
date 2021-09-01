af7e6b2180ff97ca1ef65215c961318a1ddb1e26
Agustin Delgado <delgadoagustin147@gmail.com>
2021-09-01 18:44:54 -0300

    Revert "Revert "Crea""
    This reverts commit f45502bb47165dfbce8adbad933d642a36b418da.

---
+++ TP3/TP3/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    public class Pedido
+    {
+        int Numero;
+        string Obs;
+        Cliente cliente;
+        string Estado;
+    }
+}
