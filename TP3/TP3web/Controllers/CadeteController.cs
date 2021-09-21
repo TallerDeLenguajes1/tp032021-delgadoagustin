032fc4ba8249745d12bd15f8bfd27d9977047fc2
Agustin Delgado <delgadoagustin147@gmail.com>
2021-09-21 20:35:53 -0300

    Cadete Controller-View

---
+++ TP3/TP3web/Controllers/CadeteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TP3;
+
+namespace TP3web.Controllers
+{
+    public class CadeteController : Controller
+    {
+        private readonly ILogger<CadeteController> _logger;
+
+        public CadeteController(ILogger<CadeteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Cliente> Clientes = new();
+            for (int i = 0; i < 5; i++)
+            {
+                Cliente cl = new()
+                {
+                    Nombre = "Agustin",
+                    Direccion = "Gorriti",
+                    Telefono = "1234",
+                    Id = i
+                };
+                Clientes.Add(cl);
+            }
+            
+           
+            return View(Clientes);
+        }
+    }
+}
