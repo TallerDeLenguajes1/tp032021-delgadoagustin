5251646fd1ea276deec0d2ec5c6f872c0e7a6762
Agustin Delgado <delgadoagustin147@gmail.com>
2021-09-22 09:16:53 -0300

    Constructor Sobrecargado Pedido

--- TP3/TP3/Pedido.cs
+++ TP3/TP3/Pedido.cs
@@ -17,5 +17,13 @@ namespace TP3
         public string Obs { get => obs; set => obs = value; }
         public Cliente Cliente { get => cliente; set => cliente = value; }
         public string Estado { get => estado; set => estado = value; }
+
+        public Pedido(int n,string o,string e,int id_c,string nombre_c, string direccion_c, string telefono_c)
+        {
+            numero = n;
+            obs = o;
+            estado = e;
+            cliente = new Cliente(id_c, nombre_c, direccion_c, telefono_c);
+        }
     }
 }
