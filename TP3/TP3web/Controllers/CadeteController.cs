c3abcfdb9451d072a83c0ef3064a59606bfb9a0b
Agustin Delgado <delgadoagustin147@gmail.com>
2021-10-13 21:38:44 -0300

    Algunos try-catch y log

--- TP3/TP3web/Controllers/CadeteController.cs
+++ TP3/TP3web/Controllers/CadeteController.cs
@@ -16,6 +16,7 @@ namespace TP3web.Controllers
         public CadeteController(ILogger<CadeteController> logger, Base BaseDeDatos)
         {
             _logger = logger;
+            _logger.LogDebug(1, "NLog injected into Cadete Controller");
             baseDeDatos = BaseDeDatos;
         }
 
@@ -26,47 +27,85 @@ namespace TP3web.Controllers
 
         public IActionResult addCadete(string nombre,string direccion,string telefono)
         {
-            int id = 0;
-            if (baseDeDatos.cadeteria.listaCadetes.Count() > 0)
+            try
             {
-                id = baseDeDatos.cadeteria.listaCadetes.Max(x => x.Id)+1;
+                int id = 0;
+                if (baseDeDatos.cadeteria.listaCadetes.Count() > 0)
+                {
+                    id = baseDeDatos.cadeteria.listaCadetes.Max(x => x.Id) + 1;
+                }
+
+                Cadete cad = new()
+                {
+                    Nombre = nombre,
+                    Direccion = direccion,
+                    Telefono = telefono,
+                    Id = id
+                };
+                baseDeDatos.cadeteria.listaCadetes.Add(cad);
+                baseDeDatos.GuardarCadeteria();
+                _logger.LogInformation("Usuario Id: {0} Creado", id);
             }
-           
-            Cadete cad = new()
+            catch (Exception exception)
             {
-                Nombre = nombre,
-                Direccion = direccion,
-                Telefono = telefono,
-                Id = id
-            };
-            baseDeDatos.cadeteria.listaCadetes.Add(cad);
-            baseDeDatos.GuardarCadeteria();
+                _logger.LogError(exception, "Error al Agregar Cadete");
+                exception.ToString();
+            }
+            
+
             return RedirectToAction("ListarCadetes");
         }
         public IActionResult deleteCadete(int id_cad)
         {
-            Cadete cadeteToRemove = baseDeDatos.cadeteria.listaCadetes.Single(x => x.Id == id_cad);
-            baseDeDatos.cadeteria.listaCadetes.Remove(cadeteToRemove);
-            baseDeDatos.GuardarCadeteria();
+            try
+            {
+                Cadete cadeteToRemove = baseDeDatos.cadeteria.listaCadetes.Single(x => x.Id == id_cad);
+                baseDeDatos.cadeteria.listaCadetes.Remove(cadeteToRemove);
+                baseDeDatos.GuardarCadeteria();
+                _logger.LogInformation("Usuario Id: {0} Borrado", id_cad);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error al Borrar Cadete");
+                exception.ToString();
+            }
+
             return RedirectToAction("ListarCadetes");
         }
 
 
         public IActionResult addPedido(int numero, string observacion, string estado, int cad_id, int id_c, string nombre_c, string direccion_c, string telefono_c)
         {
-            Pedido ped = new Pedido(numero, observacion, estado, id_c, nombre_c, direccion_c, telefono_c);
-            baseDeDatos.cadeteria.listaCadetes.Find(x => x.Id == cad_id).agregarPedido(ped);
-            baseDeDatos.GuardarCadeteria();
+            try
+            {
+                Pedido ped = new Pedido(numero, observacion, estado, id_c, nombre_c, direccion_c, telefono_c);
+                baseDeDatos.cadeteria.listaCadetes.Find(x => x.Id == cad_id).agregarPedido(ped);
+                baseDeDatos.GuardarCadeteria();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "Error al Agregar Pedido");
+                exception.ToString();
+            }
+
             return RedirectToAction("ListarCadetes");
         }
 
         public IActionResult updateCadete(int id_cad,string nombre,string direccion,string telefono)
         {
-            Cadete cadeteToUpdate = baseDeDatos.cadeteria.listaCadetes.Single(x => x.Id == id_cad);
-            cadeteToUpdate.Nombre = nombre;
-            cadeteToUpdate.Direccion = direccion;
-            cadeteToUpdate.Telefono = telefono;
-            baseDeDatos.GuardarCadeteria();
+            try
+            {
+                Cadete cadeteToUpdate = baseDeDatos.cadeteria.listaCadetes.Single(x => x.Id == id_cad);
+                cadeteToUpdate.Nombre = nombre;
+                cadeteToUpdate.Direccion = direccion;
+                cadeteToUpdate.Telefono = telefono;
+                baseDeDatos.GuardarCadeteria();
+            }
+            catch (Exception exception)
+            {
+                exception.ToString();
+            }
+
             return RedirectToAction("ListarCadetes");
     }
 
